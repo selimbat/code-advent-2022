78b08e0ea0fe19773562c5c150e3b26f8c6218c6
Selim <selim.tirellil@hotmail.fr>
2022-12-01 12:54:13 +0100

    solve day 1

--- 1/main.ts
+++ 1/main.ts
@@ -1,8 +1,27 @@
 import fs from 'fs';
 
-const allFileContents = fs.readFileSync('broadband.sql', 'utf-8');
-allFileContents.split(/\r?\n/).forEach(line => {
-    console.log(`Line from file: ${line}`);
+const allFileContents = fs.readFileSync('./1/input.txt', 'utf-8');
+
+let maxCalories = 0;
+let elfWithMaxCal = -1;
+
+let currentElf = 0;
+let currentElfCalories = 0;
+
+allFileContents.split(/\r?\n/).forEach((line, i) => {
+    if (!line) {
+        console.log(`elf ${currentElf} end at line ${i + 1}`);
+        // end of elf inventory
+        if (currentElfCalories >= maxCalories) {
+            elfWithMaxCal = currentElf;
+            maxCalories = currentElfCalories;
+        }
+        currentElf += 1;
+        currentElfCalories = 0;
+        return;
+    }
+    // listing elf inventory
+    currentElfCalories += parseInt(line, 10);
 });
-const used = process.memoryUsage().heapUsed / 1024 / 1024;
-console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+
+console.log(`The elf with the max calories is ${elfWithMaxCal} with a total of ${maxCalories} calories.`);
