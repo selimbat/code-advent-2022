f552e7db6517a959c916dc8087f0c4a138f8b0a6
Selim <selim.tirellil@hotmail.fr>
2022-12-06 17:55:12 +0100

    solve day 4

---
+++ 4/bonus.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+
+const allFileContents = fs.readFileSync('./4/input.txt', 'utf-8');
+
+let total = 0;
+
+const rangesOverlap = (start1: number, end1: number, start2: number, end2: number): boolean => {
+    return end2 >= start1 && start2 <= end1
+}
+
+allFileContents.split(/\r?\n/).forEach((line) => {
+    if (!line) {
+        return;
+    }
+    if (line.indexOf(',') === -1) {
+        console.error('Invalid input');
+        return;
+    }
+    const [range1, range2] = line.split(',');
+    const [start1, end1] = range1.split('-').map(str => parseInt(str, 10));
+    const [start2, end2] = range2.split('-').map(str => parseInt(str, 10));
+
+    if (end1 < start1 || end2 < start2) console.log('invalid input')
+
+    if (
+        rangesOverlap(start2, end2, start1, end1)
+    ) {
+        total++;
+    }
+
+});
+
+console.log(total);
