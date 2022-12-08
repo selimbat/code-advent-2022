81a3feca808b623942657aa86e4d86b36f5abd4c
Selim <selim.tirellil@hotmail.fr>
2022-12-08 10:31:54 +0100

    solve day 6

---
+++ 6/main.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+
+const allFileContents = fs.readFileSync('./6/input.txt', 'utf-8');
+
+const lastFourElements: string[] = []
+
+let foundMarker = false;
+
+Array.from(allFileContents).forEach((e, i) => {
+    if (foundMarker) return;
+    while (lastFourElements.length >= 4) {
+        lastFourElements.shift();
+    }
+    lastFourElements.push(e);
+    const lastFourElementsSet = new Set(lastFourElements);
+    if (lastFourElementsSet.size === 4) {
+        foundMarker = true;
+        console.log(i + 1);
+    }
+})
